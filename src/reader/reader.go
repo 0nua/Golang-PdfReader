f0e18b5172bda2ec9c2446f3777ea3cea7c90152
Prisada Vladimir <vladimir-prisada@yandex.ru>
2019-01-04 17:45:08 +0300

    Initial commit

---
+++ src/reader/reader.go
@@ -0,0 +1,21 @@
+package reader
+
+import "ledongthuc/pdf"
+
+func getContent(name string) string {
+	file, reader, err := pdf.Open("./pdf/" + name)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	text := ""
+	for i := 1; i <= reader.NumPage(); i++ {
+		content := reader.Page(i).Content().Text
+		for _, item := range content {
+			text += item.S
+		}
+	}
+
+	return text
+}
